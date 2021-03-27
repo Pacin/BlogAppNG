5e6db685cabb3ffae81f1086516a85f030ef08a2
Alper Pacin <alperpacin@gmail.com>
2021-03-27 22:56:13 +0300

    added delete post feature

--- src/app/pages/blog-detail-page/blog-detail-page.component.ts
+++ src/app/pages/blog-detail-page/blog-detail-page.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-blog-detail-page',
@@ -12,7 +13,9 @@ export class BlogDetailPageComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private http: HttpClient
+    private http: HttpClient,
+    private authService: AuthService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -23,4 +26,16 @@ export class BlogDetailPageComponent implements OnInit {
     })
   }
 
+  get isAuthenticated() {
+    return !!this.authService.jwt;
+  }
+
+  deletePost(postId) {
+    this.http.delete(`http://localhost:1337/posts/${postId}`, {
+      headers: {
+        Authorization: `Bearer ${this.authService.jwt}`
+      }
+    })
+      .subscribe(() => this.router.navigateByUrl('/blogs'))
+  }
 }
